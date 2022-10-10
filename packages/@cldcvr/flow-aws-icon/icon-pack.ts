656d76a2fa9dbf00f19222ea5745ec63cfcca0d2
vikas-cldcvr <vikas@cldcvr.com>
2022-10-10 16:16:43 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-aws-icon/icon-pack.ts
+++ packages/@cldcvr/flow-aws-icon/icon-pack.ts
@@ -1,61 +1,61 @@
-import aws_database_rds_on_vmware from './svg/aws-database-rds-on-vmware';
-import aws_database_documentdb from './svg/aws-database-documentdb';
-import aws_database_keyspaces from './svg/aws-database-keyspaces';
-import aws_database_rds from './svg/aws-database-rds';
-import aws_database_quantum_ledger_database from './svg/aws-database-quantum-ledger-database';
 import aws_database_timestream from './svg/aws-database-timestream';
-import aws_database_elasticcache from './svg/aws-database-elasticcache';
+import aws_database_keyspaces from './svg/aws-database-keyspaces';
 import aws_database_neptune from './svg/aws-database-neptune';
-import aws_database_dynamodb from './svg/aws-database-dynamodb';
 import aws_database_aurora from './svg/aws-database-aurora';
-import aws_analytics_athena from './svg/aws-analytics-athena';
-import aws_analytics_glue_elastic_views from './svg/aws-analytics-glue-elastic-views';
-import aws_analytics_finspace from './svg/aws-analytics-finspace';
-import aws_analytics_kinesis from './svg/aws-analytics-kinesis';
-import aws_analytics_lake_formation from './svg/aws-analytics-lake-formation';
+import aws_database_quantum_ledger_database from './svg/aws-database-quantum-ledger-database';
+import aws_database_documentdb from './svg/aws-database-documentdb';
+import aws_database_rds_on_vmware from './svg/aws-database-rds-on-vmware';
+import aws_database_rds from './svg/aws-database-rds';
+import aws_database_elasticcache from './svg/aws-database-elasticcache';
+import aws_database_dynamodb from './svg/aws-database-dynamodb';
+import aws_analytics_quicksight from './svg/aws-analytics-quicksight';
 import aws_analytics_glue_databrew from './svg/aws-analytics-glue-databrew';
 import aws_analytics_elasticsearch_service from './svg/aws-analytics-elasticsearch-service';
-import aws_analytics_quicksight from './svg/aws-analytics-quicksight';
-import aws_analytics_cloudsearch from './svg/aws-analytics-cloudsearch';
+import aws_analytics_lake_formation from './svg/aws-analytics-lake-formation';
+import aws_analytics_glue_elastic_views from './svg/aws-analytics-glue-elastic-views';
 import aws_analytics_emr from './svg/aws-analytics-emr';
+import aws_analytics_cloudsearch from './svg/aws-analytics-cloudsearch';
+import aws_analytics_kinesis from './svg/aws-analytics-kinesis';
+import aws_analytics_athena from './svg/aws-analytics-athena';
+import aws_analytics_finspace from './svg/aws-analytics-finspace';
 import aws_storage_elastic_block_store from './svg/aws-storage-elastic-block-store';
+import aws_storage_S3 from './svg/aws-storage-S3';
 import aws_analytics_glue from './svg/aws-analytics-glue';
 import aws_storage_windows_file_server from './svg/aws-storage-windows-file-server';
-import aws_storage_glacier from './svg/aws-storage-glacier';
-import aws_storage_simple_storage_service from './svg/aws-storage-simple-storage-service';
-import aws_storage_elastic_file_system from './svg/aws-storage-elastic-file-system';
 import aws_analytics_data_pipeline from './svg/aws-analytics-data-pipeline';
+import aws_storage_simple_storage_service from './svg/aws-storage-simple-storage-service';
 import aws_analytics_redshift from './svg/aws-analytics-redshift';
-import aws_storage_S3 from './svg/aws-storage-S3';
+import aws_storage_glacier from './svg/aws-storage-glacier';
+import aws_storage_elastic_file_system from './svg/aws-storage-elastic-file-system';
 const IconPack = {
-    'aws-database-rds-on-vmware': aws_database_rds_on_vmware,
-    'aws-database-documentdb': aws_database_documentdb,
-    'aws-database-keyspaces': aws_database_keyspaces,
-    'aws-database-rds': aws_database_rds,
-    'aws-database-quantum-ledger-database': aws_database_quantum_ledger_database,
     'aws-database-timestream': aws_database_timestream,
-    'aws-database-elasticcache': aws_database_elasticcache,
+    'aws-database-keyspaces': aws_database_keyspaces,
     'aws-database-neptune': aws_database_neptune,
-    'aws-database-dynamodb': aws_database_dynamodb,
     'aws-database-aurora': aws_database_aurora,
-    'aws-analytics-athena': aws_analytics_athena,
-    'aws-analytics-glue-elastic-views': aws_analytics_glue_elastic_views,
-    'aws-analytics-finspace': aws_analytics_finspace,
-    'aws-analytics-kinesis': aws_analytics_kinesis,
-    'aws-analytics-lake-formation': aws_analytics_lake_formation,
+    'aws-database-quantum-ledger-database': aws_database_quantum_ledger_database,
+    'aws-database-documentdb': aws_database_documentdb,
+    'aws-database-rds-on-vmware': aws_database_rds_on_vmware,
+    'aws-database-rds': aws_database_rds,
+    'aws-database-elasticcache': aws_database_elasticcache,
+    'aws-database-dynamodb': aws_database_dynamodb,
+    'aws-analytics-quicksight': aws_analytics_quicksight,
     'aws-analytics-glue-databrew': aws_analytics_glue_databrew,
     'aws-analytics-elasticsearch-service': aws_analytics_elasticsearch_service,
-    'aws-analytics-quicksight': aws_analytics_quicksight,
-    'aws-analytics-cloudsearch': aws_analytics_cloudsearch,
+    'aws-analytics-lake-formation': aws_analytics_lake_formation,
+    'aws-analytics-glue-elastic-views': aws_analytics_glue_elastic_views,
     'aws-analytics-emr': aws_analytics_emr,
+    'aws-analytics-cloudsearch': aws_analytics_cloudsearch,
+    'aws-analytics-kinesis': aws_analytics_kinesis,
+    'aws-analytics-athena': aws_analytics_athena,
+    'aws-analytics-finspace': aws_analytics_finspace,
     'aws-storage-elastic-block-store': aws_storage_elastic_block_store,
+    'aws-storage-S3': aws_storage_S3,
     'aws-analytics-glue': aws_analytics_glue,
     'aws-storage-windows-file-server': aws_storage_windows_file_server,
-    'aws-storage-glacier': aws_storage_glacier,
-    'aws-storage-simple-storage-service': aws_storage_simple_storage_service,
-    'aws-storage-elastic-file-system': aws_storage_elastic_file_system,
     'aws-analytics-data-pipeline': aws_analytics_data_pipeline,
+    'aws-storage-simple-storage-service': aws_storage_simple_storage_service,
     'aws-analytics-redshift': aws_analytics_redshift,
-    'aws-storage-S3': aws_storage_S3,
+    'aws-storage-glacier': aws_storage_glacier,
+    'aws-storage-elastic-file-system': aws_storage_elastic_file_system,
 } as Record<string, string>;
 export default IconPack;
--- packages/@cldcvr/flow-product-icon/icon-pack.ts
+++ packages/@cldcvr/flow-product-icon/icon-pack.ts
@@ -1,81 +1,81 @@
+import p_docker from './svg/p-docker';
+import p_google from './svg/p-google';
 import p_gcp from './svg/p-gcp';
+import p_aws_light from './svg/p-aws-light';
 import p_azure from './svg/p-azure';
+import p_aws_dark from './svg/p-aws-dark';
 import p_gitlab from './svg/p-gitlab';
 import p_kubernetes from './svg/p-kubernetes';
-import p_aws_light from './svg/p-aws-light';
-import p_docker from './svg/p-docker';
-import p_aws_dark from './svg/p-aws-dark';
-import p_google from './svg/p-google';
 import p_cloudcover_dark_full from './svg/p-cloudcover-dark-full';
-import p_oracle from './svg/p-oracle';
-import p_slack from './svg/p-slack';
-import p_asana from './svg/p-asana';
-import l_mongodb from './svg/l-mongodb';
 import p_jira from './svg/p-jira';
-import p_mongodb from './svg/p-mongodb';
+import p_asana from './svg/p-asana';
 import p_sql_server from './svg/p-sql-server';
 import p_postgresql from './svg/p-postgresql';
+import l_mongodb from './svg/l-mongodb';
+import p_slack from './svg/p-slack';
+import p_mongodb from './svg/p-mongodb';
+import p_oracle from './svg/p-oracle';
 import p_mysql from './svg/p-mysql';
 import p_cloudcover_dark from './svg/p-cloudcover-dark';
-import p_discord from './svg/p-discord';
-import p_golang from './svg/p-golang';
-import p_tomcat from './svg/p-tomcat';
-import p_prometheus from './svg/p-prometheus';
 import p_dataiku from './svg/p-dataiku';
-import p_jupyter from './svg/p-jupyter';
 import p_tableau from './svg/p-tableau';
+import p_prometheus from './svg/p-prometheus';
 import p_datapipes from './svg/p-datapipes';
-import p_hadoop from './svg/p-hadoop';
+import p_golang from './svg/p-golang';
+import p_discord from './svg/p-discord';
+import p_jupyter from './svg/p-jupyter';
+import p_tomcat from './svg/p-tomcat';
 import p_postmark from './svg/p-postmark';
-import p_fortify from './svg/p-fortify';
+import p_hadoop from './svg/p-hadoop';
 import p_sonarcloud from './svg/p-sonarcloud';
-import p_terraform from './svg/p-terraform';
-import p_sonarqube_light from './svg/p-sonarqube-light';
-import p_redis from './svg/p-redis';
 import p_memcache from './svg/p-memcache';
 import p_sonarqube_dark from './svg/p-sonarqube-dark';
 import p_github from './svg/p-github';
-import p_sonatype from './svg/p-sonatype';
+import p_sonarqube_light from './svg/p-sonarqube-light';
 import p_vscode from './svg/p-vscode';
+import p_fortify from './svg/p-fortify';
+import p_redis from './svg/p-redis';
+import p_sonatype from './svg/p-sonatype';
+import p_terraform from './svg/p-terraform';
 const IconPack = {
+    'p-docker': p_docker,
+    'p-google': p_google,
     'p-gcp': p_gcp,
+    'p-aws-light': p_aws_light,
     'p-azure': p_azure,
+    'p-aws-dark': p_aws_dark,
     'p-gitlab': p_gitlab,
     'p-kubernetes': p_kubernetes,
-    'p-aws-light': p_aws_light,
-    'p-docker': p_docker,
-    'p-aws-dark': p_aws_dark,
-    'p-google': p_google,
     'p-cloudcover-dark-full': p_cloudcover_dark_full,
-    'p-oracle': p_oracle,
-    'p-slack': p_slack,
-    'p-asana': p_asana,
-    'l-mongodb': l_mongodb,
     'p-jira': p_jira,
-    'p-mongodb': p_mongodb,
+    'p-asana': p_asana,
     'p-sql-server': p_sql_server,
     'p-postgresql': p_postgresql,
+    'l-mongodb': l_mongodb,
+    'p-slack': p_slack,
+    'p-mongodb': p_mongodb,
+    'p-oracle': p_oracle,
     'p-mysql': p_mysql,
     'p-cloudcover-dark': p_cloudcover_dark,
-    'p-discord': p_discord,
-    'p-golang': p_golang,
-    'p-tomcat': p_tomcat,
-    'p-prometheus': p_prometheus,
     'p-dataiku': p_dataiku,
-    'p-jupyter': p_jupyter,
     'p-tableau': p_tableau,
+    'p-prometheus': p_prometheus,
     'p-datapipes': p_datapipes,
-    'p-hadoop': p_hadoop,
+    'p-golang': p_golang,
+    'p-discord': p_discord,
+    'p-jupyter': p_jupyter,
+    'p-tomcat': p_tomcat,
     'p-postmark': p_postmark,
-    'p-fortify': p_fortify,
+    'p-hadoop': p_hadoop,
     'p-sonarcloud': p_sonarcloud,
-    'p-terraform': p_terraform,
-    'p-sonarqube-light': p_sonarqube_light,
-    'p-redis': p_redis,
     'p-memcache': p_memcache,
     'p-sonarqube-dark': p_sonarqube_dark,
     'p-github': p_github,
-    'p-sonatype': p_sonatype,
+    'p-sonarqube-light': p_sonarqube_light,
     'p-vscode': p_vscode,
+    'p-fortify': p_fortify,
+    'p-redis': p_redis,
+    'p-sonatype': p_sonatype,
+    'p-terraform': p_terraform,
 } as Record<string, string>;
 export default IconPack;
