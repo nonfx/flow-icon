d7822893b4b98b0782432a069c6156e5af2cee79
vikas-cldcvr <vikas@cldcvr.com>
2023-01-02 13:21:32 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-product-icon/icon-pack.ts
+++ packages/@cldcvr/flow-product-icon/icon-pack.ts
@@ -1,82 +1,82 @@
-import p_gcp from './svg/p-gcp';
-import p_azure from './svg/p-azure';
 import p_aws_light from './svg/p-aws-light';
-import p_google from './svg/p-google';
-import p_docker from './svg/p-docker';
-import p_aws_dark from './svg/p-aws-dark';
-import p_cloudcover_dark_full from './svg/p-cloudcover-dark-full';
+import p_gcp from './svg/p-gcp';
 import p_kubernetes from './svg/p-kubernetes';
+import p_azure from './svg/p-azure';
+import p_aws_dark from './svg/p-aws-dark';
+import p_docker from './svg/p-docker';
 import p_gitlab from './svg/p-gitlab';
+import p_google from './svg/p-google';
+import p_cloudcover_dark_full from './svg/p-cloudcover-dark-full';
 import p_jira from './svg/p-jira';
-import p_mongodb from './svg/p-mongodb';
 import p_sql_server from './svg/p-sql-server';
-import p_slack from './svg/p-slack';
-import l_mongodb from './svg/l-mongodb';
 import p_oracle from './svg/p-oracle';
-import p_cloudcover_light from './svg/p-cloudcover-light';
-import p_mysql from './svg/p-mysql';
 import p_postgresql from './svg/p-postgresql';
+import l_mongodb from './svg/l-mongodb';
+import p_slack from './svg/p-slack';
+import p_cloudcover_light from './svg/p-cloudcover-light';
+import p_mongodb from './svg/p-mongodb';
+import p_mysql from './svg/p-mysql';
 import p_cloudcover_dark from './svg/p-cloudcover-dark';
-import p_tomcat from './svg/p-tomcat';
+import p_golang from './svg/p-golang';
 import p_asana from './svg/p-asana';
 import p_dataiku from './svg/p-dataiku';
-import p_tableau from './svg/p-tableau';
-import p_datapipes from './svg/p-datapipes';
-import p_jupyter from './svg/p-jupyter';
-import p_golang from './svg/p-golang';
-import p_discord from './svg/p-discord';
 import p_prometheus from './svg/p-prometheus';
+import p_jupyter from './svg/p-jupyter';
+import p_tableau from './svg/p-tableau';
+import p_discord from './svg/p-discord';
+import p_tomcat from './svg/p-tomcat';
+import p_datapipes from './svg/p-datapipes';
 import p_postmark from './svg/p-postmark';
-import p_vscode from './svg/p-vscode';
-import p_github from './svg/p-github';
-import p_sonarqube_light from './svg/p-sonarqube-light';
 import p_sonatype from './svg/p-sonatype';
+import p_sonarqube_light from './svg/p-sonarqube-light';
 import p_sonarcloud from './svg/p-sonarcloud';
 import p_sonarqube_dark from './svg/p-sonarqube-dark';
-import p_fortify from './svg/p-fortify';
-import p_redis from './svg/p-redis';
+import p_vscode from './svg/p-vscode';
 import p_memcache from './svg/p-memcache';
+import p_fortify from './svg/p-fortify';
+import p_github from './svg/p-github';
+import p_redis from './svg/p-redis';
 import p_hadoop from './svg/p-hadoop';
 import p_terraform from './svg/p-terraform';
 const IconPack = {
-    'p-gcp': p_gcp,
-    'p-azure': p_azure,
     'p-aws-light': p_aws_light,
-    'p-google': p_google,
-    'p-docker': p_docker,
-    'p-aws-dark': p_aws_dark,
-    'p-cloudcover-dark-full': p_cloudcover_dark_full,
+    'p-gcp': p_gcp,
     'p-kubernetes': p_kubernetes,
+    'p-azure': p_azure,
+    'p-aws-dark': p_aws_dark,
+    'p-docker': p_docker,
     'p-gitlab': p_gitlab,
+    'p-google': p_google,
+    'p-cloudcover-dark-full': p_cloudcover_dark_full,
     'p-jira': p_jira,
-    'p-mongodb': p_mongodb,
     'p-sql-server': p_sql_server,
-    'p-slack': p_slack,
-    'l-mongodb': l_mongodb,
     'p-oracle': p_oracle,
-    'p-cloudcover-light': p_cloudcover_light,
-    'p-mysql': p_mysql,
     'p-postgresql': p_postgresql,
+    'l-mongodb': l_mongodb,
+    'p-slack': p_slack,
+    'p-cloudcover-light': p_cloudcover_light,
+    'p-mongodb': p_mongodb,
+    'p-mysql': p_mysql,
     'p-cloudcover-dark': p_cloudcover_dark,
-    'p-tomcat': p_tomcat,
+    'p-golang': p_golang,
     'p-asana': p_asana,
     'p-dataiku': p_dataiku,
-    'p-tableau': p_tableau,
-    'p-datapipes': p_datapipes,
-    'p-jupyter': p_jupyter,
-    'p-golang': p_golang,
-    'p-discord': p_discord,
     'p-prometheus': p_prometheus,
+    'p-jupyter': p_jupyter,
+    'p-tableau': p_tableau,
+    'p-discord': p_discord,
+    'p-tomcat': p_tomcat,
+    'p-datapipes': p_datapipes,
     'p-postmark': p_postmark,
-    'p-vscode': p_vscode,
-    'p-github': p_github,
-    'p-sonarqube-light': p_sonarqube_light,
     'p-sonatype': p_sonatype,
+    'p-sonarqube-light': p_sonarqube_light,
     'p-sonarcloud': p_sonarcloud,
     'p-sonarqube-dark': p_sonarqube_dark,
-    'p-fortify': p_fortify,
-    'p-redis': p_redis,
+    'p-vscode': p_vscode,
     'p-memcache': p_memcache,
+    'p-fortify': p_fortify,
+    'p-github': p_github,
+    'p-redis': p_redis,
     'p-hadoop': p_hadoop,
     'p-terraform': p_terraform,
 } as Record<string, string>;
--- packages/@cldcvr/flow-system-icon/svg/i-project-shape.ts
+++ packages/@cldcvr/flow-system-icon/svg/i-project-shape.ts
@@ -1,5 +1,5 @@
 import { html } from "lit-html"; 
  export default html`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect x="1" y="1" width="22" height="22" rx="4" fill="white"/>
+<path d="M1 5C1 2.79086 2.79086 1 5 1H19C21.2091 1 23 2.79086 23 5V19C23 21.2091 21.2091 23 19 23H5C2.79086 23 1 21.2091 1 19V5Z" fill="white"/>
 </svg>
 `.strings.join("");
