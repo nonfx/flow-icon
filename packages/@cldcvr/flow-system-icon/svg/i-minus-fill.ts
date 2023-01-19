f3e227fa5eafd1fb4cfd58e9ffa0e7a2c5ccc1bc
vikas-cldcvr <vikas@cldcvr.com>
2023-01-19 17:41:43 +0530

    :sparkles: FLOW-599 new icons added

---
+++ packages/@cldcvr/flow-system-icon/svg/i-minus-fill.ts
@@ -0,0 +1,5 @@
+import { html } from "lit-html"; 
+ export default html`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23ZM7.875 10.625C7.11561 10.625 6.5 11.2406 6.5 12C6.5 12.7594 7.11561 13.375 7.875 13.375H16.125C16.8844 13.375 17.5 12.7594 17.5 12C17.5 11.2406 16.8844 10.625 16.125 10.625H7.875Z" fill="white"/>
+</svg>
+`.strings.join("");
