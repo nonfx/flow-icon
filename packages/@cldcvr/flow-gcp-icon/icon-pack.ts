48d7ad04005aba7ba28d949b95b72086028c3470
vikas-cldcvr <vikas@cldcvr.com>
2023-03-27 14:44:43 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-gcp-icon/icon-pack.ts
+++ packages/@cldcvr/flow-gcp-icon/icon-pack.ts
@@ -1,21 +1,21 @@
-import gcp_database_bigquery from './svg/gcp-database-bigquery';
-import gcp_storage_gcs from './svg/gcp-storage-gcs';
-import gcp_database_data_lab from './svg/gcp-database-data-lab';
-import gcp_container_engine from './svg/gcp-container-engine';
-import gcp_cloud_functions from './svg/gcp-cloud-functions';
-import gcp_cloud_spanner from './svg/gcp-cloud-spanner';
-import gcp_database_data_studio from './svg/gcp-database-data-studio';
 import gcp_cloud_sql from './svg/gcp-cloud-sql';
+import gcp_database_data_studio from './svg/gcp-database-data-studio';
+import gcp_container_engine from './svg/gcp-container-engine';
+import gcp_database_data_lab from './svg/gcp-database-data-lab';
+import gcp_storage_gcs from './svg/gcp-storage-gcs';
 import gcp_database_cloud_composer from './svg/gcp-database-cloud-composer';
+import gcp_cloud_spanner from './svg/gcp-cloud-spanner';
+import gcp_cloud_functions from './svg/gcp-cloud-functions';
+import gcp_database_bigquery from './svg/gcp-database-bigquery';
 const IconPack = {
-    'gcp-database-bigquery': gcp_database_bigquery,
-    'gcp-storage-gcs': gcp_storage_gcs,
-    'gcp-database-data-lab': gcp_database_data_lab,
-    'gcp-container-engine': gcp_container_engine,
-    'gcp-cloud-functions': gcp_cloud_functions,
-    'gcp-cloud-spanner': gcp_cloud_spanner,
-    'gcp-database-data-studio': gcp_database_data_studio,
     'gcp-cloud-sql': gcp_cloud_sql,
+    'gcp-database-data-studio': gcp_database_data_studio,
+    'gcp-container-engine': gcp_container_engine,
+    'gcp-database-data-lab': gcp_database_data_lab,
+    'gcp-storage-gcs': gcp_storage_gcs,
     'gcp-database-cloud-composer': gcp_database_cloud_composer,
+    'gcp-cloud-spanner': gcp_cloud_spanner,
+    'gcp-cloud-functions': gcp_cloud_functions,
+    'gcp-database-bigquery': gcp_database_bigquery,
 } as Record<string, string>;
 export default IconPack;
