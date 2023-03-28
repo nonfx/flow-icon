c00bef4c9b2de39022c003e457c15894a3f84b45
vikas-cldcvr <vikas@cldcvr.com>
2023-03-28 05:45:06 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-gcp-icon/icon-pack.ts
+++ packages/@cldcvr/flow-gcp-icon/icon-pack.ts
@@ -1,21 +1,21 @@
 import gcp_storage_gcs from './svg/gcp-storage-gcs';
-import gcp_database_data_lab from './svg/gcp-database-data-lab';
-import gcp_database_data_studio from './svg/gcp-database-data-studio';
-import gcp_cloud_spanner from './svg/gcp-cloud-spanner';
-import gcp_cloud_functions from './svg/gcp-cloud-functions';
-import gcp_cloud_sql from './svg/gcp-cloud-sql';
 import gcp_database_cloud_composer from './svg/gcp-database-cloud-composer';
+import gcp_database_data_studio from './svg/gcp-database-data-studio';
 import gcp_database_bigquery from './svg/gcp-database-bigquery';
+import gcp_cloud_functions from './svg/gcp-cloud-functions';
+import gcp_cloud_spanner from './svg/gcp-cloud-spanner';
+import gcp_cloud_sql from './svg/gcp-cloud-sql';
+import gcp_database_data_lab from './svg/gcp-database-data-lab';
 import gcp_container_engine from './svg/gcp-container-engine';
 const IconPack = {
     'gcp-storage-gcs': gcp_storage_gcs,
-    'gcp-database-data-lab': gcp_database_data_lab,
-    'gcp-database-data-studio': gcp_database_data_studio,
-    'gcp-cloud-spanner': gcp_cloud_spanner,
-    'gcp-cloud-functions': gcp_cloud_functions,
-    'gcp-cloud-sql': gcp_cloud_sql,
     'gcp-database-cloud-composer': gcp_database_cloud_composer,
+    'gcp-database-data-studio': gcp_database_data_studio,
     'gcp-database-bigquery': gcp_database_bigquery,
+    'gcp-cloud-functions': gcp_cloud_functions,
+    'gcp-cloud-spanner': gcp_cloud_spanner,
+    'gcp-cloud-sql': gcp_cloud_sql,
+    'gcp-database-data-lab': gcp_database_data_lab,
     'gcp-container-engine': gcp_container_engine,
 } as Record<string, string>;
 export default IconPack;
