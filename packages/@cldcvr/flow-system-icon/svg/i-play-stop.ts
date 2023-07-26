cd041cab7848157178abced04389bd8509760353
PragyanGour213 <vikas@cldcvr.com>
2023-07-26 07:14:15 +0000

    Icons updated by GithubActions

---
+++ packages/@cldcvr/flow-system-icon/svg/i-play-stop.ts
@@ -0,0 +1,16 @@
+export default `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_984_867)">
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M12 24C18.6274 24 24 18.6274 24 12C24 5.37258 18.6274 0 12 0C5.37258 0 0 5.37258 0 12C0 18.6274 5.37258 24 12 24ZM9 7.5C8.17157 7.5 7.5 8.17157 7.5 9V15C7.5 15.8284 8.17157 16.5 9 16.5H15C15.8284 16.5 16.5 15.8284 16.5 15V9C16.5 8.17157 15.8284 7.5 15 7.5H9Z"
+            fill="white"
+        />
+    </g>
+    <defs>
+        <clipPath id="clip0_984_867">
+            <rect width="24" height="24" fill="white" />
+        </clipPath>
+    </defs>
+</svg>
+`;
