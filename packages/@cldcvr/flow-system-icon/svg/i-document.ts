3a23824f4026a309d8cba14b7edabff7db1a4867
PragyanGour213 <vikas@cldcvr.com>
2023-10-06 08:29:59 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-system-icon/svg/i-document.ts
+++ packages/@cldcvr/flow-system-icon/svg/i-document.ts
@@ -1,9 +1,16 @@
 export default `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M3 3C3 1.34315 4.34315 0 6 0H12.8787C13.6743 0 14.4374 0.31607 15 0.87868L20.1213 6C20.6839 6.56261 21 7.32567 21 8.12132V21C21 22.6569 19.6569 24 18 24H6C4.34314 24 3 22.6569 3 21V3Z"
-        fill="white"
-    />
+    <g clip-path="url(#clip0_984_749)">
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M3 3C3 1.34315 4.34315 0 6 0H12.8787C13.6743 0 14.4374 0.31607 15 0.87868L20.1213 6C20.6839 6.56261 21 7.32567 21 8.12132V21C21 22.6569 19.6569 24 18 24H6C4.34314 24 3 22.6569 3 21V3Z"
+            fill="white"
+        />
+    </g>
+    <defs>
+        <clipPath id="clip0_984_749">
+            <rect width="24" height="24" fill="white" />
+        </clipPath>
+    </defs>
 </svg>
 `;
--- packages/@cldcvr/flow-system-icon/svg/i-map.ts
+++ packages/@cldcvr/flow-system-icon/svg/i-map.ts
@@ -1,21 +1,28 @@
 export default `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M15.2616 0L9.55695 5.70463V24L15.2616 18.2954V0Z"
-        fill="white"
-    />
-    <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M3.4346 2.4346C3.02673 2.02673 2.41331 1.90471 1.88039 2.12545C1.34747 2.34619 1 2.86622 1 3.44305V17.7046C1 18.0829 1.15026 18.4456 1.41771 18.7131L6.70463 24V5.70463L3.4346 2.4346Z"
-        fill="white"
-    />
-    <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M23.4008 5.28692L18.1139 0V18.2954L21.3839 21.5654C21.7918 21.9733 22.4052 22.0953 22.9381 21.8745C23.4711 21.6538 23.8185 21.1338 23.8185 20.5569V6.29537C23.8185 5.91713 23.6683 5.55438 23.4008 5.28692Z"
-        fill="white"
-    />
+    <g clip-path="url(#clip0_984_955)">
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M15.2616 0L9.55695 5.70463V24L15.2616 18.2954V0Z"
+            fill="white"
+        />
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M3.4346 2.4346C3.02673 2.02673 2.41331 1.90471 1.88039 2.12545C1.34747 2.34619 1 2.86622 1 3.44305V17.7046C1 18.0829 1.15026 18.4456 1.41771 18.7131L6.70463 24V5.70463L3.4346 2.4346Z"
+            fill="white"
+        />
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M23.4008 5.28692L18.1139 0V18.2954L21.3839 21.5654C21.7918 21.9733 22.4052 22.0953 22.9381 21.8745C23.4711 21.6538 23.8185 21.1338 23.8185 20.5569V6.29537C23.8185 5.91713 23.6683 5.55438 23.4008 5.28692Z"
+            fill="white"
+        />
+    </g>
+    <defs>
+        <clipPath id="clip0_984_955">
+            <rect width="24" height="24" fill="white" />
+        </clipPath>
+    </defs>
 </svg>
 `;
