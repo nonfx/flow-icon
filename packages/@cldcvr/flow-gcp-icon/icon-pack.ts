84032cffc6149867c82d184b7deb53f9e30c18b1
vikas-cldcvr <vikas@cldcvr.com>
2023-10-23 07:40:41 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-gcp-icon/icon-pack.ts
+++ packages/@cldcvr/flow-gcp-icon/icon-pack.ts
@@ -1,21 +1,23 @@
-import gcp_database_data_lab from './svg/gcp-database-data-lab';
 import gcp_database_data_studio from './svg/gcp-database-data-studio';
-import gcp_cloud_functions from './svg/gcp-cloud-functions';
-import gcp_database_bigquery from './svg/gcp-database-bigquery';
-import gcp_storage_gcs from './svg/gcp-storage-gcs';
-import gcp_container_engine from './svg/gcp-container-engine';
 import gcp_database_cloud_composer from './svg/gcp-database-cloud-composer';
-import gcp_cloud_spanner from './svg/gcp-cloud-spanner';
+import gcp_database_bigquery from './svg/gcp-database-bigquery';
+import gcp_database_data_lab from './svg/gcp-database-data-lab';
 import gcp_cloud_sql from './svg/gcp-cloud-sql';
+import gcp_cloud_spanner from './svg/gcp-cloud-spanner';
+import gcp_storage_gcs from './svg/gcp-storage-gcs';
+import gcp_cloud_functions from './svg/gcp-cloud-functions';
+import gcp_test_GCP from './svg/gcp-test_GCP';
+import gcp_container_engine from './svg/gcp-container-engine';
 const IconPack = {
-    'gcp-database-data-lab': gcp_database_data_lab,
     'gcp-database-data-studio': gcp_database_data_studio,
-    'gcp-cloud-functions': gcp_cloud_functions,
-    'gcp-database-bigquery': gcp_database_bigquery,
-    'gcp-storage-gcs': gcp_storage_gcs,
-    'gcp-container-engine': gcp_container_engine,
     'gcp-database-cloud-composer': gcp_database_cloud_composer,
-    'gcp-cloud-spanner': gcp_cloud_spanner,
+    'gcp-database-bigquery': gcp_database_bigquery,
+    'gcp-database-data-lab': gcp_database_data_lab,
     'gcp-cloud-sql': gcp_cloud_sql,
+    'gcp-cloud-spanner': gcp_cloud_spanner,
+    'gcp-storage-gcs': gcp_storage_gcs,
+    'gcp-cloud-functions': gcp_cloud_functions,
+    'gcp-test_GCP': gcp_test_GCP,
+    'gcp-container-engine': gcp_container_engine,
 } as Record<string, string>;
 export default IconPack;
