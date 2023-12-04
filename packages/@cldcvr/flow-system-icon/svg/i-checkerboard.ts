089bd0a4bdf23d9eca0525ca2eb6692336e83cab
PragyanGour213 <vikas@cldcvr.com>
2023-12-04 10:13:48 +0000

    Icons updated by GithubActions

---
+++ packages/@cldcvr/flow-system-icon/svg/i-checkerboard.ts
@@ -0,0 +1,18 @@
+export default `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g opacity="0.8">
+        <path d="M4.8 0H0V4.8H4.8V0Z" fill="white" />
+        <path d="M9.60005 4.80005L4.8 4.8L4.80005 9.60005H9.60005V4.80005Z" fill="white" />
+        <path d="M19.1999 4.80005H14.3999V9.60005H19.1999V4.80005Z" fill="white" />
+        <path d="M14.4001 0H9.6001L9.60005 4.80005H14.3999L14.4001 0Z" fill="white" />
+        <path d="M24 0H19.2L19.1999 4.80005L24 4.8V0Z" fill="white" />
+        <path d="M4.80005 9.60005L0 9.59998V14.4H4.8L4.80005 9.60005Z" fill="white" />
+        <path d="M9.60005 14.4L4.8 14.4L4.80005 19.2H9.60005V14.4Z" fill="white" />
+        <path d="M19.1999 14.4H14.3999V19.2H19.1999V14.4Z" fill="white" />
+        <path d="M14.3999 9.60005H9.60005V14.4H14.3999V9.60005Z" fill="white" />
+        <path d="M24 9.59998L19.1999 9.60005V14.4L24 14.4V9.59998Z" fill="white" />
+        <path d="M4.80005 19.2L0 19.2V24H4.8L4.80005 19.2Z" fill="white" />
+        <path d="M14.3999 19.2H9.60005L9.6001 24H14.4001L14.3999 19.2Z" fill="white" />
+        <path d="M24 19.2L19.1999 19.2L19.2 24H24V19.2Z" fill="white" />
+    </g>
+</svg>
+`;
