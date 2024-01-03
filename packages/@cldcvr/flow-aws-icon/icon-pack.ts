a855eca9b99e5e0f8d1de3e5b29618429af87b57
PragyanGour213 <vikas@cldcvr.com>
2024-01-03 13:23:55 +0000

    Icons updated by GithubActions

--- packages/@cldcvr/flow-aws-icon/icon-pack.ts
+++ packages/@cldcvr/flow-aws-icon/icon-pack.ts
@@ -1,67 +1,83 @@
-import aws_database_documentdb from './svg/aws-database-documentdb';
-import aws_database_aurora from './svg/aws-database-aurora';
-import aws_database_rds from './svg/aws-database-rds';
-import aws_database_keyspaces from './svg/aws-database-keyspaces';
+import aws_database_quantum_ledger_database from './svg/aws-database-quantum-ledger-database';
 import aws_database_elasticcache from './svg/aws-database-elasticcache';
-import aws_database_neptune from './svg/aws-database-neptune';
-import aws_database_dynamodb from './svg/aws-database-dynamodb';
 import aws_database_timestream from './svg/aws-database-timestream';
 import aws_database_rds_on_vmware from './svg/aws-database-rds-on-vmware';
-import aws_database_quantum_ledger_database from './svg/aws-database-quantum-ledger-database';
-import aws_analytics_cloudsearch from './svg/aws-analytics-cloudsearch';
-import aws_analytics_finspace from './svg/aws-analytics-finspace';
-import aws_analytics_quicksight from './svg/aws-analytics-quicksight';
-import aws_analytics_glue_databrew from './svg/aws-analytics-glue-databrew';
-import aws_analytics_lake_formation from './svg/aws-analytics-lake-formation';
+import aws_database_documentdb from './svg/aws-database-documentdb';
+import aws_database_aurora from './svg/aws-database-aurora';
+import aws_database_dynamodb from './svg/aws-database-dynamodb';
+import aws_database_neptune from './svg/aws-database-neptune';
+import aws_database_rds from './svg/aws-database-rds';
+import aws_database_keyspaces from './svg/aws-database-keyspaces';
 import aws_analytics_elasticsearch_service from './svg/aws-analytics-elasticsearch-service';
-import aws_analytics_emr from './svg/aws-analytics-emr';
-import aws_analytics_kinesis from './svg/aws-analytics-kinesis';
+import aws_analytics_cloudsearch from './svg/aws-analytics-cloudsearch';
 import aws_analytics_glue_elastic_views from './svg/aws-analytics-glue-elastic-views';
+import aws_analytics_lake_formation from './svg/aws-analytics-lake-formation';
+import aws_analytics_glue_databrew from './svg/aws-analytics-glue-databrew';
+import aws_analytics_emr from './svg/aws-analytics-emr';
+import aws_analytics_finspace from './svg/aws-analytics-finspace';
 import aws_analytics_athena from './svg/aws-analytics-athena';
-import aws_analytics_glue from './svg/aws-analytics-glue';
-import aws_storage_elastic_file_system from './svg/aws-storage-elastic-file-system';
-import aws_storage_elastic_block_store from './svg/aws-storage-elastic-block-store';
-import aws_storage_windows_file_server from './svg/aws-storage-windows-file-server';
-import aws_storage_simple_storage_service from './svg/aws-storage-simple-storage-service';
-import aws_analytics_redshift from './svg/aws-analytics-redshift';
+import aws_analytics_quicksight from './svg/aws-analytics-quicksight';
+import aws_analytics_kinesis from './svg/aws-analytics-kinesis';
 import aws_analytics_data_pipeline from './svg/aws-analytics-data-pipeline';
-import aws_storage_glacier from './svg/aws-storage-glacier';
-import aws_sagemaker from './svg/aws-sagemaker';
+import aws_storage_elastic_file_system from './svg/aws-storage-elastic-file-system';
+import aws_analytics_redshift from './svg/aws-analytics-redshift';
+import aws_storage_windows_file_server from './svg/aws-storage-windows-file-server';
+import aws_analytics_glue from './svg/aws-analytics-glue';
+import aws_storage_elastic_block_store from './svg/aws-storage-elastic-block-store';
 import aws_storage_S3 from './svg/aws-storage-S3';
+import aws_iot_core from './svg/aws-iot-core';
+import aws_storage_simple_storage_service from './svg/aws-storage-simple-storage-service';
+import aws_storage_glacier from './svg/aws-storage-glacier';
+import aws_iot_analytics from './svg/aws-iot-analytics';
+import aws_iot_greengrass from './svg/aws-iot-greengrass';
+import aws_EKS from './svg/aws-EKS';
 import aws_quicksight from './svg/aws-quicksight';
+import aws_comprehend from './svg/aws-comprehend';
+import aws_sagemaker from './svg/aws-sagemaker';
+import aws_deeplens from './svg/aws-deeplens';
 import aws_appstream from './svg/aws-appstream';
+import aws_ECS from './svg/aws-ECS';
+import aws_copilot from './svg/aws-copilot';
 const IconPack = {
-    'aws-database-documentdb': aws_database_documentdb,
-    'aws-database-aurora': aws_database_aurora,
-    'aws-database-rds': aws_database_rds,
-    'aws-database-keyspaces': aws_database_keyspaces,
+    'aws-database-quantum-ledger-database': aws_database_quantum_ledger_database,
     'aws-database-elasticcache': aws_database_elasticcache,
-    'aws-database-neptune': aws_database_neptune,
-    'aws-database-dynamodb': aws_database_dynamodb,
     'aws-database-timestream': aws_database_timestream,
     'aws-database-rds-on-vmware': aws_database_rds_on_vmware,
-    'aws-database-quantum-ledger-database': aws_database_quantum_ledger_database,
-    'aws-analytics-cloudsearch': aws_analytics_cloudsearch,
-    'aws-analytics-finspace': aws_analytics_finspace,
-    'aws-analytics-quicksight': aws_analytics_quicksight,
-    'aws-analytics-glue-databrew': aws_analytics_glue_databrew,
-    'aws-analytics-lake-formation': aws_analytics_lake_formation,
+    'aws-database-documentdb': aws_database_documentdb,
+    'aws-database-aurora': aws_database_aurora,
+    'aws-database-dynamodb': aws_database_dynamodb,
+    'aws-database-neptune': aws_database_neptune,
+    'aws-database-rds': aws_database_rds,
+    'aws-database-keyspaces': aws_database_keyspaces,
     'aws-analytics-elasticsearch-service': aws_analytics_elasticsearch_service,
-    'aws-analytics-emr': aws_analytics_emr,
-    'aws-analytics-kinesis': aws_analytics_kinesis,
+    'aws-analytics-cloudsearch': aws_analytics_cloudsearch,
     'aws-analytics-glue-elastic-views': aws_analytics_glue_elastic_views,
+    'aws-analytics-lake-formation': aws_analytics_lake_formation,
+    'aws-analytics-glue-databrew': aws_analytics_glue_databrew,
+    'aws-analytics-emr': aws_analytics_emr,
+    'aws-analytics-finspace': aws_analytics_finspace,
     'aws-analytics-athena': aws_analytics_athena,
-    'aws-analytics-glue': aws_analytics_glue,
-    'aws-storage-elastic-file-system': aws_storage_elastic_file_system,
-    'aws-storage-elastic-block-store': aws_storage_elastic_block_store,
-    'aws-storage-windows-file-server': aws_storage_windows_file_server,
-    'aws-storage-simple-storage-service': aws_storage_simple_storage_service,
-    'aws-analytics-redshift': aws_analytics_redshift,
+    'aws-analytics-quicksight': aws_analytics_quicksight,
+    'aws-analytics-kinesis': aws_analytics_kinesis,
     'aws-analytics-data-pipeline': aws_analytics_data_pipeline,
-    'aws-storage-glacier': aws_storage_glacier,
-    'aws-sagemaker': aws_sagemaker,
+    'aws-storage-elastic-file-system': aws_storage_elastic_file_system,
+    'aws-analytics-redshift': aws_analytics_redshift,
+    'aws-storage-windows-file-server': aws_storage_windows_file_server,
+    'aws-analytics-glue': aws_analytics_glue,
+    'aws-storage-elastic-block-store': aws_storage_elastic_block_store,
     'aws-storage-S3': aws_storage_S3,
+    'aws-iot-core': aws_iot_core,
+    'aws-storage-simple-storage-service': aws_storage_simple_storage_service,
+    'aws-storage-glacier': aws_storage_glacier,
+    'aws-iot-analytics': aws_iot_analytics,
+    'aws-iot-greengrass': aws_iot_greengrass,
+    'aws-EKS': aws_EKS,
     'aws-quicksight': aws_quicksight,
+    'aws-comprehend': aws_comprehend,
+    'aws-sagemaker': aws_sagemaker,
+    'aws-deeplens': aws_deeplens,
     'aws-appstream': aws_appstream,
+    'aws-ECS': aws_ECS,
+    'aws-copilot': aws_copilot,
 } as Record<string, string>;
 export default IconPack;
