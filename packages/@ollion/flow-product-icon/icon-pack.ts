c07cf79adadbc246e897aa39af0611c90d1e6d62
PragyanGour213 <vikas@ollion.com>
2024-01-19 08:07:16 +0000

    Icons updated by GithubActions

--- packages/@ollion/flow-product-icon/icon-pack.ts
+++ packages/@ollion/flow-product-icon/icon-pack.ts
@@ -1,95 +1,97 @@
-import p_google from './svg/p-google';
-import p_gcp from './svg/p-gcp';
-import p_azure_1 from './svg/p-azure-1';
 import p_aws_dark from './svg/p-aws-dark';
-import p_azure from './svg/p-azure';
-import p_aws_light from './svg/p-aws-light';
-import p_gitlab from './svg/p-gitlab';
-import p_kubernetes from './svg/p-kubernetes';
-import p_cloudcover_dark_full from './svg/p-cloudcover-dark-full';
 import p_docker from './svg/p-docker';
-import p_mongodb from './svg/p-mongodb';
-import p_oracle from './svg/p-oracle';
+import p_kubernetes from './svg/p-kubernetes';
+import p_gcp from './svg/p-gcp';
+import p_google from './svg/p-google';
+import p_aws_light from './svg/p-aws-light';
+import p_azure_1 from './svg/p-azure-1';
+import p_gitlab from './svg/p-gitlab';
+import p_azure from './svg/p-azure';
+import p_cloudcover_dark_full from './svg/p-cloudcover-dark-full';
 import l_mongodb from './svg/l-mongodb';
-import p_jira from './svg/p-jira';
-import p_sql_server from './svg/p-sql-server';
 import p_postgresql from './svg/p-postgresql';
 import p_slack from './svg/p-slack';
+import p_mongodb from './svg/p-mongodb';
+import p_oracle from './svg/p-oracle';
+import p_sql_server from './svg/p-sql-server';
+import p_jira from './svg/p-jira';
 import p_mysql from './svg/p-mysql';
 import p_cloudcover_light from './svg/p-cloudcover-light';
 import p_cloudcover_dark from './svg/p-cloudcover-dark';
-import p_asana from './svg/p-asana';
-import p_dataiku from './svg/p-dataiku';
 import p_jupyter from './svg/p-jupyter';
-import p_prometheus from './svg/p-prometheus';
-import p_golang from './svg/p-golang';
-import p_datapipes from './svg/p-datapipes';
 import p_discord from './svg/p-discord';
-import p_tomcat from './svg/p-tomcat';
+import p_asana from './svg/p-asana';
 import p_tableau from './svg/p-tableau';
+import p_prometheus from './svg/p-prometheus';
+import p_datapipes from './svg/p-datapipes';
+import p_dataiku from './svg/p-dataiku';
+import p_tomcat from './svg/p-tomcat';
+import p_golang from './svg/p-golang';
 import p_postmark from './svg/p-postmark';
-import p_vscode from './svg/p-vscode';
-import p_sonarcloud from './svg/p-sonarcloud';
-import p_redis from './svg/p-redis';
-import p_sonarqube_light from './svg/p-sonarqube-light';
-import p_sonarqube_dark from './svg/p-sonarqube-dark';
-import p_sonatype from './svg/p-sonatype';
-import p_fortify from './svg/p-fortify';
 import p_memcache from './svg/p-memcache';
-import p_hadoop from './svg/p-hadoop';
+import p_redis from './svg/p-redis';
+import p_vscode from './svg/p-vscode';
+import p_sonarqube_light from './svg/p-sonarqube-light';
+import p_sonarcloud from './svg/p-sonarcloud';
+import p_sonarqube_dark from './svg/p-sonarqube-dark';
+import p_fortify from './svg/p-fortify';
+import p_sonatype from './svg/p-sonatype';
 import p_github from './svg/p-github';
-import p_kafka_light from './svg/p-kafka-light';
-import p_terraform from './svg/p-terraform';
-import p_cloudcover_light_full from './svg/p-cloudcover-light-full';
+import p_hadoop from './svg/p-hadoop';
 import p_kafka_dark from './svg/p-kafka-dark';
 import p_salesforce from './svg/p-salesforce';
 import p_snowflake from './svg/p-snowflake';
+import p_terraform from './svg/p-terraform';
+import p_kafka_light from './svg/p-kafka-light';
+import p_trino from './svg/p-trino';
+import p_cloudcover_light_full from './svg/p-cloudcover-light-full';
 const IconPack = {
-    'p-google': p_google,
-    'p-gcp': p_gcp,
-    'p-azure-1': p_azure_1,
     'p-aws-dark': p_aws_dark,
-    'p-azure': p_azure,
-    'p-aws-light': p_aws_light,
-    'p-gitlab': p_gitlab,
-    'p-kubernetes': p_kubernetes,
-    'p-cloudcover-dark-full': p_cloudcover_dark_full,
     'p-docker': p_docker,
-    'p-mongodb': p_mongodb,
-    'p-oracle': p_oracle,
+    'p-kubernetes': p_kubernetes,
+    'p-gcp': p_gcp,
+    'p-google': p_google,
+    'p-aws-light': p_aws_light,
+    'p-azure-1': p_azure_1,
+    'p-gitlab': p_gitlab,
+    'p-azure': p_azure,
+    'p-cloudcover-dark-full': p_cloudcover_dark_full,
     'l-mongodb': l_mongodb,
-    'p-jira': p_jira,
-    'p-sql-server': p_sql_server,
     'p-postgresql': p_postgresql,
     'p-slack': p_slack,
+    'p-mongodb': p_mongodb,
+    'p-oracle': p_oracle,
+    'p-sql-server': p_sql_server,
+    'p-jira': p_jira,
     'p-mysql': p_mysql,
     'p-cloudcover-light': p_cloudcover_light,
     'p-cloudcover-dark': p_cloudcover_dark,
-    'p-asana': p_asana,
-    'p-dataiku': p_dataiku,
     'p-jupyter': p_jupyter,
-    'p-prometheus': p_prometheus,
-    'p-golang': p_golang,
-    'p-datapipes': p_datapipes,
     'p-discord': p_discord,
-    'p-tomcat': p_tomcat,
+    'p-asana': p_asana,
     'p-tableau': p_tableau,
+    'p-prometheus': p_prometheus,
+    'p-datapipes': p_datapipes,
+    'p-dataiku': p_dataiku,
+    'p-tomcat': p_tomcat,
+    'p-golang': p_golang,
     'p-postmark': p_postmark,
-    'p-vscode': p_vscode,
-    'p-sonarcloud': p_sonarcloud,
-    'p-redis': p_redis,
-    'p-sonarqube-light': p_sonarqube_light,
-    'p-sonarqube-dark': p_sonarqube_dark,
-    'p-sonatype': p_sonatype,
-    'p-fortify': p_fortify,
     'p-memcache': p_memcache,
-    'p-hadoop': p_hadoop,
+    'p-redis': p_redis,
+    'p-vscode': p_vscode,
+    'p-sonarqube-light': p_sonarqube_light,
+    'p-sonarcloud': p_sonarcloud,
+    'p-sonarqube-dark': p_sonarqube_dark,
+    'p-fortify': p_fortify,
+    'p-sonatype': p_sonatype,
     'p-github': p_github,
-    'p-kafka-light': p_kafka_light,
-    'p-terraform': p_terraform,
-    'p-cloudcover-light-full': p_cloudcover_light_full,
+    'p-hadoop': p_hadoop,
     'p-kafka-dark': p_kafka_dark,
     'p-salesforce': p_salesforce,
     'p-snowflake': p_snowflake,
+    'p-terraform': p_terraform,
+    'p-kafka-light': p_kafka_light,
+    'p-trino': p_trino,
+    'p-cloudcover-light-full': p_cloudcover_light_full,
 } as Record<string, string>;
 export default IconPack;
