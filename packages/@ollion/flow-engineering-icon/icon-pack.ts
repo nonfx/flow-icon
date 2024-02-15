42bb74bd44376799c8f6ffbf83ec85a09c370035
PragyanGour213 <vikas@ollion.com>
2024-02-15 12:11:11 +0000

    Icons updated by GithubActions

--- packages/@ollion/flow-engineering-icon/icon-pack.ts
+++ packages/@ollion/flow-engineering-icon/icon-pack.ts
@@ -1,47 +1,47 @@
-import i_name from './svg/i-name';
-import i_category from './svg/i-category';
-import i_division from './svg/i-division';
-import i_justification from './svg/i-justification';
-import i_domain from './svg/i-domain';
-import i_class from './svg/i-class';
-import i_canonical from './svg/i-canonical';
 import i_type from './svg/i-type';
-import i_description from './svg/i-description';
+import i_class from './svg/i-class';
+import i_name from './svg/i-name';
+import i_division from './svg/i-division';
+import i_canonical from './svg/i-canonical';
+import i_category from './svg/i-category';
 import i_family from './svg/i-family';
-import i_notarization from './svg/i-notarization';
-import i_onboarding from './svg/i-onboarding';
-import i_assets from './svg/i-assets';
+import i_domain from './svg/i-domain';
+import i_justification from './svg/i-justification';
+import i_description from './svg/i-description';
 import i_summary from './svg/i-summary';
-import i_component from './svg/i-component';
-import i_evidence from './svg/i-evidence';
-import i_attachment_2 from './svg/i-attachment-2';
 import i_classification from './svg/i-classification';
-import i_catalog from './svg/i-catalog';
 import i_copy_classification from './svg/i-copy-classification';
+import i_assets from './svg/i-assets';
+import i_onboarding from './svg/i-onboarding';
+import i_attachment_2 from './svg/i-attachment-2';
+import i_evidence from './svg/i-evidence';
+import i_component from './svg/i-component';
+import i_catalog from './svg/i-catalog';
+import i_notarization from './svg/i-notarization';
 import i_save_test from './svg/i-save-test';
 import i_tenant from './svg/i-tenant';
 import i_admin from './svg/i-admin';
 const IconPack = {
-    'i-name': i_name,
-    'i-category': i_category,
-    'i-division': i_division,
-    'i-justification': i_justification,
-    'i-domain': i_domain,
-    'i-class': i_class,
-    'i-canonical': i_canonical,
     'i-type': i_type,
-    'i-description': i_description,
+    'i-class': i_class,
+    'i-name': i_name,
+    'i-division': i_division,
+    'i-canonical': i_canonical,
+    'i-category': i_category,
     'i-family': i_family,
-    'i-notarization': i_notarization,
-    'i-onboarding': i_onboarding,
-    'i-assets': i_assets,
+    'i-domain': i_domain,
+    'i-justification': i_justification,
+    'i-description': i_description,
     'i-summary': i_summary,
-    'i-component': i_component,
-    'i-evidence': i_evidence,
-    'i-attachment-2': i_attachment_2,
     'i-classification': i_classification,
-    'i-catalog': i_catalog,
     'i-copy-classification': i_copy_classification,
+    'i-assets': i_assets,
+    'i-onboarding': i_onboarding,
+    'i-attachment-2': i_attachment_2,
+    'i-evidence': i_evidence,
+    'i-component': i_component,
+    'i-catalog': i_catalog,
+    'i-notarization': i_notarization,
     'i-save-test': i_save_test,
     'i-tenant': i_tenant,
     'i-admin': i_admin,
