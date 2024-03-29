9e9e643f2fc582ca216dad898b344998825e0b64
PragyanGour213 <vikas@ollion.com>
2024-03-29 06:03:02 +0000

    Icons updated by GithubActions

--- packages/@ollion/flow-engineering-icon/icon-pack.ts
+++ packages/@ollion/flow-engineering-icon/icon-pack.ts
@@ -1,73 +1,85 @@
-import i_division from './svg/i-division';
 import i_name from './svg/i-name';
-import i_description from './svg/i-description';
-import i_domain from './svg/i-domain';
-import i_type from './svg/i-type';
 import i_category from './svg/i-category';
-import i_family from './svg/i-family';
+import i_division from './svg/i-division';
 import i_canonical from './svg/i-canonical';
 import i_class from './svg/i-class';
+import i_type from './svg/i-type';
+import i_family from './svg/i-family';
+import i_domain from './svg/i-domain';
+import i_description from './svg/i-description';
 import i_justification from './svg/i-justification';
-import i_classification from './svg/i-classification';
-import i_catalog from './svg/i-catalog';
 import i_attachment_2 from './svg/i-attachment-2';
-import i_assets from './svg/i-assets';
-import i_notarization from './svg/i-notarization';
-import i_copy_classification from './svg/i-copy-classification';
+import i_catalog from './svg/i-catalog';
 import i_onboarding from './svg/i-onboarding';
-import i_evidence from './svg/i-evidence';
-import i_summary from './svg/i-summary';
+import i_notarization from './svg/i-notarization';
 import i_component from './svg/i-component';
-import i_tenant from './svg/i-tenant';
-import i_map from './svg/i-map';
-import i_save_test from './svg/i-save-test';
-import i_admin from './svg/i-admin';
-import i_policy_cis from './svg/i-policy-cis';
-import i_policy_pci from './svg/i-policy-pci';
+import i_copy_classification from './svg/i-copy-classification';
+import i_summary from './svg/i-summary';
+import i_evidence from './svg/i-evidence';
+import i_classification from './svg/i-classification';
+import i_assets from './svg/i-assets';
 import i_service from './svg/i-service';
+import i_map from './svg/i-map';
+import i_policy_cis from './svg/i-policy-cis';
+import i_admin from './svg/i-admin';
+import i_tenant from './svg/i-tenant';
+import i_save_test from './svg/i-save-test';
+import i_policy_pci from './svg/i-policy-pci';
 import i_policy_nist from './svg/i-policy-nist';
 import i_policy_gdpr from './svg/i-policy-gdpr';
 import i_policy_soc2 from './svg/i-policy-soc2';
+import i_medium_severity from './svg/i-medium-severity';
+import i_software from './svg/i-software';
+import i_evidence_approved from './svg/i-evidence-approved';
+import i_high_severity from './svg/i-high-severity';
+import i_low_severity from './svg/i-low-severity';
 import i_subline_of_business from './svg/i-subline-of-business';
 import i_function from './svg/i-function';
 import i_line_of_business from './svg/i-line-of-business';
+import i_evidence_rejected from './svg/i-evidence-rejected';
 import i_department from './svg/i-department';
-import i_software from './svg/i-software';
+import i_evidence_missing from './svg/i-evidence-missing';
 const IconPack = {
-    'i-division': i_division,
     'i-name': i_name,
-    'i-description': i_description,
-    'i-domain': i_domain,
-    'i-type': i_type,
     'i-category': i_category,
-    'i-family': i_family,
+    'i-division': i_division,
     'i-canonical': i_canonical,
     'i-class': i_class,
+    'i-type': i_type,
+    'i-family': i_family,
+    'i-domain': i_domain,
+    'i-description': i_description,
     'i-justification': i_justification,
-    'i-classification': i_classification,
-    'i-catalog': i_catalog,
     'i-attachment-2': i_attachment_2,
-    'i-assets': i_assets,
-    'i-notarization': i_notarization,
-    'i-copy-classification': i_copy_classification,
+    'i-catalog': i_catalog,
     'i-onboarding': i_onboarding,
-    'i-evidence': i_evidence,
-    'i-summary': i_summary,
+    'i-notarization': i_notarization,
     'i-component': i_component,
-    'i-tenant': i_tenant,
-    'i-map': i_map,
-    'i-save-test': i_save_test,
-    'i-admin': i_admin,
-    'i-policy-cis': i_policy_cis,
-    'i-policy-pci': i_policy_pci,
+    'i-copy-classification': i_copy_classification,
+    'i-summary': i_summary,
+    'i-evidence': i_evidence,
+    'i-classification': i_classification,
+    'i-assets': i_assets,
     'i-service': i_service,
+    'i-map': i_map,
+    'i-policy-cis': i_policy_cis,
+    'i-admin': i_admin,
+    'i-tenant': i_tenant,
+    'i-save-test': i_save_test,
+    'i-policy-pci': i_policy_pci,
     'i-policy-nist': i_policy_nist,
     'i-policy-gdpr': i_policy_gdpr,
     'i-policy-soc2': i_policy_soc2,
+    'i-medium-severity': i_medium_severity,
+    'i-software': i_software,
+    'i-evidence-approved': i_evidence_approved,
+    'i-high-severity': i_high_severity,
+    'i-low-severity': i_low_severity,
     'i-subline-of-business': i_subline_of_business,
     'i-function': i_function,
     'i-line-of-business': i_line_of_business,
+    'i-evidence-rejected': i_evidence_rejected,
     'i-department': i_department,
-    'i-software': i_software,
+    'i-evidence-missing': i_evidence_missing,
 } as Record<string, string>;
 export default IconPack;
